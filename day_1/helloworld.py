83284734957a8f3bca263cd4da6a470af6e98e0c
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-08-30 23:56:17 -0300

    Día 1: helloworld.py (olvidé subirlo)

---
+++ day_1/helloworld.py
@@ -0,0 +1,34 @@
+import sys
+
+print('1. Version Python')
+print('Version:', sys.version)
+
+print('\n2. Operations')
+print('Suma', 2 + 5)
+print('Resta', 2 - 5)
+print('Multiplicación', 3 * 2)
+print('Módulo', 4 % 2)
+print('División', 4 / 2)
+print('Exponencial', 3 ** 3)
+print('División entera', 43 // 2)
+
+print('\n3. Strings')
+name = 'Javier'
+family_name = 'Rodriguez'
+country = 'Argentina'
+msg = 'I am enjoying 30 days of python'
+
+print(name)
+print(family_name)
+print(country)
+print(msg)
+
+print('\n4. Data types')
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(name))
+print(type(family_name))
+print(type(country))
